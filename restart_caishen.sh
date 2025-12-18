79afdd76b434afdcc2731882e5665af6344015be
root <root@vps136344.whmpanels.com>
2025-12-18 19:20:02 +0000

    added restart_caishen.sh

---
+++ restart_caishen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+cd /var/www/caishen
+git pull origin main
+
+cd services/web-dapp
+npm install
+npm run build
+
+rsync -av --delete out/ /var/www/caishen/web/
+
+cd /var/www/caishen
+nginx -t
+systemctl reload nginx
+
+docker-compose down -v
+docker-compose up -d --build
+docker-compose logs -f
